98f77ea3bfd81eebaaabda6b160958b47684fb3a
namanda <matcelous@gmail.com>
2021-05-11 10:05:17 +0300

    first commit

---
+++ Desktop/vaultApplication/vaultApplication/VaultApplication.cs
@@ -0,0 +1,18 @@
+using MFiles.VAF;
+using MFiles.VAF.Common;
+using MFiles.VAF.Configuration;
+using MFilesAPI;
+using System;
+using System.Diagnostics;
+
+namespace vaultApplication
+{
+    /// <summary>
+    /// The entry point for this Vault Application Framework application.
+    /// </summary>
+    /// <remarks>Examples and further information available on the developer portal: http://developer.m-files.com/. </remarks>
+    public class VaultApplication
+        : VaultApplicationBase
+    {
+    }
+}
